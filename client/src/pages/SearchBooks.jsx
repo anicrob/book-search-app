e1a175c834b9410eb5fa1404f3cb4850fbda932b
goose <anrobbins18@gmail.com>
2023-08-14 19:22:16 -0400

    recent updates

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -82,11 +82,7 @@ const SearchBooks = () => {
 
     try {
 
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      saveBook(bookToSave, token);
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
